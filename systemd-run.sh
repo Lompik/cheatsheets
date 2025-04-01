fadd2e6ab928a46917482d0dee8043863d3a4658
Stephane Fontaine <stefont@gmail.com>
2025-04-01 15:21:48 +0400

    [systemd-run] add more examples

--- systemd-run.sh
+++ systemd-run.sh
@@ -1,2 +1,11 @@
 # Reminder via SystemD Transient service
 systemd-run --user -E DISPLAY="$DISPLAY" --unit=reminder1 --on-active=5min notify-send -u critical 'Timers Up'
+# to set [Service] properties: ues -p
+systemd-run --property=Type=exec -p KillSignal=SIGINT -p NetworkNamespacePath=/var/run/netns/enp999ns -p MemoryMax=500M -- command
+# or --service-type=forking
+systemd-run \
+  --wait \
+  --user \
+  -E MYENVVAR=VAL \
+  --setenv=ASABOVE=VAL \
+  --unit=myprog-$(uuidgen -t)  # to set unique service name
