689d0e5d9a90039678e2ee3642ca3accb91a4f3b
Stephane Fontaine <stefont@gmail.com>
2025-04-01 15:22:12 +0400

    [fd] warn about --strip-cwd-prefix

--- perl.sh
+++ perl.sh
@@ -6,7 +6,6 @@ perl -alne 'print scalar @F, " $_"'
 # Print the total number of fields that match a pattern
 perl -alne 'map { /regex/ && $t++ } @F; END { print $t }'
 perl -alne '$t += /regex/ for @F; END { print $t }'
-perl -alne '$t += grep /regex/, @F; END { print $t
 
 # Print lines that match regex AAA followed by regex BBB followed by CCC
 perl -ne '/AAA.*BBB.*CCC/ && print'
